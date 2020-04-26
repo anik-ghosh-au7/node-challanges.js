71a4274df83dbb86176ab146adfd5b2fda284448
Anik Ghosh <tech.anikghosh@gmail.com>
2020-04-26 18:18:53 +0530

    project-1

---
+++ node-express/Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'Anik Ghosh',
+        email: 'anik@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Akash Dutta',
+        email: 'akash@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Raghav Nayak',
+        email: 'raghav@gmail.com',
+        status: 'active'
+    }
+]
+
+module.exports = members;
