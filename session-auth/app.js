6a146e8d315432afad2939a0ef7c151aeab262ab
Anik Ghosh <tech.anikghosh@gmail.com>
2020-05-14 18:21:36 +0530

    solutions

--- session-auth/app.js
+++ session-auth/app.js
@@ -1,3 +1,5 @@
+// Reference - https://www.youtube.com/watch?v=OH6Z0dJ_Huk
+
 const express = require('express');
 const session = require('express-session');
 const morgan = require('morgan');
@@ -35,7 +37,31 @@ app.use(session({
         sameSite: true,
         secure: IN_PROD
     }
-}))
+}));
+
+const redirectLogin = (req, res, next) => {
+    if (!req.session.userId) {
+        res.redirect('/Login');
+    } else {
+        next()
+    }
+};
+
+const redirectHome = (req, res, next) => {
+    if (req.session.userId) {
+        res.redirect('/Home');
+    } else {
+        next()
+    }
+};
+
+app.use((req, res, next) => {
+    const {userId} = req.session;
+    if (userId) {
+        res.locals.user = users.find(user => user.id === userId);
+    }
+    next()
+})
 
 app.get('/', (req, res) => {
     const {userId} = req.session;
@@ -53,18 +79,28 @@ app.get('/', (req, res) => {
     `)
 })
 
-app.get('/home', (req, res) => {
+app.get('/home', redirectLogin, (req, res) => {
+    const {user} = res.locals;
+    console.log(req.session);
+    console.log(req.session.id); // same as req.sessionId
     res.send(`
     <h1>Home<h1>
+    <h3>
     <a href='/'>Main</a>
+    </h3><h6>
     <ul>
-        <li>Name :</li>
-        <li>Email :</li>
+        <li>Name : ${user.name}</li>
+        <li>Email : ${user.email}</li>
     </ul>
+    </h6>
     `)
-})
+});
 
-app.get('/login', (req, res) => {
+// app.get('/profile', redirectLogin, (req, res) => {
+//     const {user} = res.locals;
+// })
+
+app.get('/login', redirectHome, redirectHome, (req, res) => {
     res.send(`
         <h1>Login</h1>
         <form method='post' action='/login'>
@@ -76,7 +112,7 @@ app.get('/login', (req, res) => {
     `)
 })
 
-app.get('/register', (req, res) => {
+app.get('/register', redirectHome, (req, res) => {
     res.send(`
         <h1>Register</h1>
         <form method='post' action='/register'>
@@ -89,16 +125,58 @@ app.get('/register', (req, res) => {
     `)
 })
 
-app.post('/login', (req, res) => {
-    
+app.post('/login', redirectHome, (req, res) => {
+    const {email, password} = req.body;
+
+    if (email && password) {
+        const user = users.find(
+            user => user.email === email && user.password === password
+            )
+
+        if (user) {
+            req.session.userId = user.id;
+            return res.redirect('/home');
+        } 
+    }
+
+    res.redirect('/login');
 })
 
-app.post('/register', (req, res) => {
-    
+app.post('/register', redirectHome, (req, res) => {
+    const {name, email, password} = req.body;
+
+    if (name && email && password) {
+        const exists = users.some(
+            user => user.email === email
+        )
+
+        if (!exists) {
+            const user = {
+                id: users.length + 1,
+                name,
+                email, 
+                password
+            }
+
+            users.push(user);
+
+            req.session.userId = user.id;
+
+            return res.redirect('/home');
+        }
+    }
+
+    res.redirect('/register');
 })
 
-app.post('/logout', (req, res) => {
-    
+app.post('/logout', redirectLogin, (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            return res.redirect('/home');
+        }
+        res.clearCookie(SESS_NAME);
+        res.redirect('/login');
+    })
 })
 
 app.listen(PORT, () => {console.log(
